d5eeb151cf4d35dba6c81402d6bf837a56999fba
THunderEX <thunderex@gmail.com>
2018-09-02 10:13:30 +0800

    move query truncate logic into pipeline and remove duplicate

--- ikea_bti/pipelines.py
+++ ikea_bti/pipelines.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+from urllib.parse import urlparse, urlunparse
+
 from scrapy.pipelines.images import ImagesPipeline
 from scrapy import Request
+from scrapy.exceptions import DropItem
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
@@ -8,8 +11,19 @@ from scrapy import Request
 
 
 class IkeaBtiPipeline(object):
+    def __init__(self):
+        super(IkeaBtiPipeline, self).__init__()
+        self.urls_seen = set()
+
     def process_item(self, item, spider):
-        return item
+        # clear query in url in case there are any vertical bar "|" which break markdown table delimiter
+        item['url'] = urlunparse(urlparse(item['url'])._replace(query=''))
+
+        if item['url'] in self.urls_seen:
+            raise DropItem("Duplicate item found: %s" % item)
+        else:
+            self.urls_seen.add(item['url'])
+            return item
 
 
 class KeepNameImgPipeline(ImagesPipeline):
